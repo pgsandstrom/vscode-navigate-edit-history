026f7b39dfbb6d4d62184e77def3db3beef5be35
Per Sandström <pg.sandstrom@gmail.com>
2019-12-03 13:20:16 +0100

    Add some fixes for newlines

--- src/extension.ts
+++ src/extension.ts
@@ -12,7 +12,8 @@ type Edit = {
 export function activate(context: vscode.ExtensionContext) {
 
 	let currentStepsBack = 0;
-	const editList: Edit[] = [];
+	let ignoreNextStepsBackReset = false;
+	let editList: Edit[] = [];
 
 	const fileSystemWatcher = vscode.workspace.createFileSystemWatcher("**/*", false, true, false);
 	const onCreate = fileSystemWatcher.onDidCreate((uri: vscode.Uri) => {
@@ -22,8 +23,12 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log(`delete: ${uri.fsPath}`);
 	});
 
-	vscode.window.onDidChangeTextEditorSelection((e:vscode.TextEditorSelectionChangeEvent) => {
-		currentStepsBack = 0;
+	vscode.window.onDidChangeTextEditorSelection((e: vscode.TextEditorSelectionChangeEvent) => {
+		if (ignoreNextStepsBackReset) {
+			ignoreNextStepsBackReset = false;
+		} else {
+			currentStepsBack = 0;
+		}
 	});
 
 	const documentChangeListener = vscode.workspace.onDidChangeTextDocument((e: vscode.TextDocumentChangeEvent) => {
@@ -32,35 +37,59 @@ export function activate(context: vscode.ExtensionContext) {
 		e.contentChanges.forEach((change) => {
 			const line = change.range.start.line;
 			const character = change.range.start.character;
-			const range =  change.rangeLength;
+			const range = change.rangeLength;
+			// console.log(`line: ${line}, ${character}, ${range},${change.range.end}, "${change.text}"`)
 
-			// console.log(`line: ${line}, ${character}, ${range},${change.range.end}, ${change.text}`)
-			
 			const lastEdit = editList[editList.length - 1];
-			if(lastEdit !== undefined && lastEdit.filepath === filepath && lastEdit.line === line) {
+			const numberOfNewlines = change.text?.match(/\n/g)?.length;
+			const startsWithNewline = change.text && (change.text[0] === '\n' || change.text.substring(0,2) === '\r\n');
+			// console.log(`startsWithNewline: ${startsWithNewline}`);
+
+			const newEdit = {
+				filepath,
+				line: line + (startsWithNewline ? 1 : 0),
+				character,
+				range,
+			};
+
+			// skip edits on same line as last one:
+			if (lastEdit !== undefined && lastEdit.filepath === filepath && lastEdit.line === line) {
 				// console.log('skipping this edit');
 				return;
 			}
 
-			editList.push({
-				filepath,
-				line,
-				character,
-				range,
-			});
 
-			if(editList.length > EDIT_LIST_MAX_LENGTH) {
-				editList.splice(0,1);
+			// adjust old edits if we add new lines:
+			if (numberOfNewlines !== undefined && numberOfNewlines > 0) {
+				// console.log(`newlines: ${numberOfNewlines}`)
+				editList = editList.map(edit => {
+					if (edit.filepath === newEdit.filepath && edit.line >= newEdit.line) {
+						return {
+							...edit,
+							line: edit.line + numberOfNewlines
+						};
+					} else {
+						return edit;
+					}
+				});
+			}
+
+
+
+			editList.push(newEdit)
+
+			if (editList.length > EDIT_LIST_MAX_LENGTH) {
+				editList.splice(0, 1);
 			}
 		});
 	});
 
 	let gotoEditCommand = vscode.commands.registerCommand('navigateEditHistory.moveCursorToPreviousEdit', () => {
-		const edit = editList[editList.length-1 - currentStepsBack];
-		// editList.forEach((e,index) => {
-		// 	console.log(`${index}: ${e.line}`);
-		// });
-		// console.log(`going to line ${edit.line}`)
+		const edit = editList[editList.length - 1 - currentStepsBack];
+		editList.forEach((e, index) => {
+			console.log(`${index}: ${e.line}`);
+		});
+		// console.log(`going to line ${edit.line}, indx ${editList.length - 1 - currentStepsBack}, stepsback ${currentStepsBack}`);
 		moveToEdit(edit);
 		currentStepsBack++;
 	});
@@ -77,6 +106,7 @@ export function activate(context: vscode.ExtensionContext) {
 			activeEditor = vscode.window.activeTextEditor!;
 		}
 
+		ignoreNextStepsBackReset = true;
 		activeEditor.selection = new vscode.Selection(edit.line, edit.character, edit.line, edit.character);
 		activeEditor.revealRange(new vscode.Range(edit.line, edit.character, edit.line, edit.character));
 	};
